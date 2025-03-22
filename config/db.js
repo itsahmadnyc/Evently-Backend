3a702e42329541390b5c853fdd0e4f3208b245f3
Muhammad Ahmad <its_ahmad_nyc@yahoo.com>
2025-03-22 11:46:50 +0500

    New DB

--- config/db.js
+++ config/db.js
@@ -1,25 +1,23 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config(); 
+require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    port: process.env.DB_PORT,
-    logging: false, 
-  }
-);
-
+const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, 
+    },
+  },
+  logging: false, 
+});
 
 (async () => {
   try {
     await sequelize.authenticate();
-    console.log('✅ Connected to MySQL successfully.');
+    console.log('✅ Connected to PostgreSQL successfully.');
   } catch (error) {
-    console.error('❌ MySQL connection failed:', error);
+    console.error('❌ PostgreSQL connection failed:', error);
   }
 })();
 
